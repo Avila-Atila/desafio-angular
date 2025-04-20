5915122fd842f4832dc682891b9c7d8f1930ab6f
Atila Garcia Avila <atilagarcia12@gmail.com>
2025-04-20 18:25:47 -0300

    começo do trabalho na home, preparação para incluir bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'desafio_angular';
+  title = 'Sistema_de_Dados_FORD';
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,24 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RequestService } from '../services/request.service';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 @Component({
   selector: 'app-home',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
 export class HomeComponent {
+  dataDeAceso: string | null = null;
+
   servico = inject(RequestService);
-  constructor(private router: Router) {}
+  constructor(private router: Router) {
+    this.dataDeAceso = new Date().toLocaleDateString('pt-br', {
+      hour: '2-digit',
+      minute: '2-digit',
+      day: '2-digit',
+      month: '2-digit',
+      year: 'numeric',
+    });
+  }
 }
